a5242157a6d4b1f6ae72ccefcc02d63ef4f51eca
Lpberends <91067257+Lpberends@users.noreply.github.com>
2022-06-18 01:19:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -101,7 +101,7 @@ var blocks = setInterval(function(){
     //Horizontal Movement
     if (inputLeft || inputRight){
         var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
-        linearMovement = inputRight * 3 - inputLeft * 3;
+        linearMovement = inputRight (2 + counter * 0.01) - inputLeft (2 + counter * 0.01);
         console.log("move me " + linearMovement)
         if(!thruHole){
             left += linearMovement;
